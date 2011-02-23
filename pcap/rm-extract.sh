0ee9f4d55d4c22730115707b3206d7c7b993df35
tmy1018@gmail.com <tmy1018@gmail.com@dca04f46-e904-defb-16ae-9be4b611e0ab>
2011-02-23 10:54:09 +0000

    testing

--- pcap/rm-extract.sh
+++ pcap/rm-extract.sh
@@ -3,7 +3,7 @@
 rm ./extract/[a-d]*
 rm ./extract/[e-j]*
 rm ./extract/[k-n]*
-rm ./extract/[p-t]*
+rm ./extract/[o-t]*
 rm ./extract/[u-z]*
 
 
--- pcap/tcptrace-extract.sh
+++ pcap/tcptrace-extract.sh
@@ -2,7 +2,11 @@
 
 tcptrace -ne --output_dir=extract *.pcap
 
-grep -h 'Referer: http://' extract/* > pcap-url-referer
+grep -h 'Referer: http://' ./extract/[a-d]* > pcap-url-referer
+grep -h 'Referer: http://' ./extract/[e-j]* > pcap-url-referer
+grep -h 'Referer: http://' ./extract/[k-n]* > pcap-url-referer
+grep -h 'Referer: http://' ./extract/[o-t]* > pcap-url-referer
+grep -h 'Referer: http://' ./extract/[u-z]* > pcap-url-referer
 
 ./pcap-url.py
 
