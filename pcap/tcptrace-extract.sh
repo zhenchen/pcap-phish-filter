75a06465c7cf5d32a9cfa20f7d7635503a880241
tmy1018@gmail.com <tmy1018@gmail.com@dca04f46-e904-defb-16ae-9be4b611e0ab>
2011-03-06 04:24:39 +0000

    modified parts concerning URL extraction

--- pcap/tcptrace-extract.sh
+++ pcap/tcptrace-extract.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-tcptrace -ne --output_dir=extract *.pcap
+time_beg=`date`
+
+tcptrace -ne --output_dir=extract class_all_*
+
+time_fin_tcptrace=`date`
 
 grep -h 'Referer: http://' ./extract/[a-d]* > pcap-url-referer
 grep -h 'Referer: http://' ./extract/[e-j]* > pcap-url-referer
@@ -8,8 +12,16 @@ grep -h 'Referer: http://' ./extract/[k-n]* > pcap-url-referer
 grep -h 'Referer: http://' ./extract/[o-t]* > pcap-url-referer
 grep -h 'Referer: http://' ./extract/[u-z]* > pcap-url-referer
 
+time_fin_grep_referer=`date`
+
 ./pcap-url.py
 
+time_fin_grep_URL=`date`
+
+echo "Start ${time_beg}"
+echo "pcap extraction finished ${time_fin_tcptrace}"
+echo "grep refere finished ${time_fin_grep_referer}"
+echo "URL extraction finished ${time_fin_grep_URL}"
 
 
 
